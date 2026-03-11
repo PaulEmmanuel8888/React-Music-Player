99f4ae8f3656288d86f05ebf9ef24ebd98566d77
Emmanuel Paul <paulemmanuel8888@gmail.com>
2026-03-11 20:15:46 +0100

    Set up Routes for the 'Playlists' and 'All Songs' Components

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,26 @@
+import { AllSongs } from "./components/AllSongs";
+import { MusicPlayer } from "./components/MusicPlayer";
+import { BrowserRouter, Routes, Route } from "react-router";
+import { Playlists } from "./components/PlayLists";
+
 function App() {
-  return <div className="app"></div>;
+  return (
+    <BrowserRouter>
+      <div className="app">
+        <main className="app-main">
+          <div className="player-section">
+            <MusicPlayer />
+          </div>
+          <div className="content-section">
+            <Routes>
+              <Route path="/" element={<AllSongs />} />
+              <Route path="/playlists" element={<Playlists />} />
+            </Routes>
+          </div>
+        </main>
+      </div>
+    </BrowserRouter>
+  );
 }
 
 export default App;
