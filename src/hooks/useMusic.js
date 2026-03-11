882aa4c57c65b97605561a6e032ddf7b908fc029
Emmanuel Paul <paulemmanuel8888@gmail.com>
2026-03-11 21:02:05 +0100

    Create custom hook to handle music logic

---
+++ src/hooks/useMusic.js
@@ -0,0 +1,31 @@
+import { useState } from "react";
+
+const songs = [
+  {
+    id: 1,
+    title: "Tension",
+    artist: "1000 Handz",
+    url: "/songs/1000 Handz - Tension.mp3",
+    duration: "2:48",
+  },
+  {
+    id: 2,
+    title: "Candy Bubbles",
+    artist: "Puma Tunes",
+    url: "/songs/Puma Tunes - Candy Bubbles (Kawaii Future Bass).mp3",
+    duration: "2:05",
+  },
+  {
+    id: 3,
+    title: "Synthwave Moon Light",
+    artist: "Sound of Freedom",
+    url: "/songs/Sound Of Freedom - Synthwave Moon Light.mp3.mp3",
+    duration: "3:11",
+  },
+];
+
+export const useMusic = () => {
+  const [allSongs, setAllSongs] = useState(songs);
+
+  return { allSongs };
+};
